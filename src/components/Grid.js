842f4ea952510e09515078631cb322d51f60e6f2
simon <scriado.f@gmail.com>
2022-05-04 09:49:07 +0200

    Displaying current guess in the grid

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Row from "./Row";
 
-export default function Grid({ currentGuess, guesses, turn }) {
+export default function Grid({ guesses, currentGuess, turn }) {
   return (
     <div>
       {guesses.map((g, i) => {
+        if (turn === i) {
+          return <Row key={i} currentGuess={currentGuess} />;
+        }
         return <Row key={i} guess={g} />;
       })}
     </div>
