cca7532660d1b3a8a9ccab126e2dc8c7626bdcd9
Simon Criado <50947741+simoncriado@users.noreply.github.com>
2022-05-12 11:06:53 +0200

    Add files via upload

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -26,8 +26,6 @@ export default function Wordle({ solution }) {
 
   return (
     <div>
-      <div>Solution - {solution}</div>
-      <div>Current guess - {currentGuess}</div>
       <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
       <Keypad usedKeys={usedKeys} />
       {showModal && (
